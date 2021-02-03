703d610972424cf703927523576ee45cf9b22807
tobi187 <fischerstobi1998@gmail.com>
2021-02-03 21:27:53 +0100

    tried implementing the add note feature

--- track_progress.py
+++ track_progress.py
@@ -44,6 +44,44 @@ with open("test.txt", "r") as fr:
     days = json.load(fr)
 
 
+def get_item(item_list, print_one_category=""):
+    for i in item_list:
+        # if print_one_day:
+        #    if print_one_day == item_list[i]["date"]:
+        #        print(i)
+        if print_one_category:
+            if print_one_category == "key":
+                print(days[i]["key"])
+            elif print_one_category == "date":
+                print(item_list[i]["date"])
+            elif print_one_category == "time_coded":
+                print(item_list[i]["time_coded"])
+            elif print_one_category == "time_videos":
+                print(item_list[i]["videos_watched"])
+        else:
+            print(i)
+
+
+# cmd_line_args = {
+#     "-s": days[0]["date"],
+#     "-g": get_item(days, sys.argv[2])
+# }
+
+
+# if f_argument == "-s":
+#     print(days[0]["date"])
+#     exit()
+# elif f_argument == "-g":
+#     get_item(days)
+#     exit()
+
+
+# for i in cmd_line_args:
+#     if f_argument == i:
+#         print(cmd_line_args[i])
+#         exit()
+
+# gen new item and update the list
 new_item = {
     "key": days[-1]["key"] + 1,
     "date": str(date.today())
@@ -62,8 +100,24 @@ if not is_unique(new_item, days):
     print("No 2nd chances")
     exit()
 
-time_coded = input("How long did you code today(in h)? ")
-time_videos = input("How long have you watched (coding)videos today(in h)? ")
+if sys.argv[1] and sys.argv[1] == "-a" or "--add":
+    if sys.argv[2] == "-n":
+        for i in days:
+            if i["date"] == str(date.today()):
+                i["note"] = []
+                for j in range(int(sys.argv[3])):
+                    note = input("note: ")
+                    i["note"].append(note)
+            elif sys.argv[3] == i["date"]:
+                if "note" not in i:
+                    i["note"] = []
+                for h in range(int(sys.argv[4])):
+                    note = input("note: ")
+                    i["note"].append(note)
+
+
+time_coded = int(input("How long did you code today(in h)? "))
+time_videos = int(input("How long have you watched (coding)videos today(in h)? "))
 
 new_item["time_coded"] = time_coded
 new_item["time_videos"] = time_videos
