2dda0ebc13d9ba25c84017f56c8b1f41208a76e8
Tobias Fischer <61592216+tobi187@users.noreply.github.com>
2021-10-14 17:38:04 +0200

    Update check_solution.py

--- check_solution.py
+++ check_solution.py
@@ -9,8 +9,8 @@ field = [
 
 field = [list(row) for row in field]
 
-moves = ["u", "d", "l"]
-
+moves_for_one_stone_left = ["r", "u", "u", "l", "l", "l", "u", "l", "d", "d", "r", "d", "l"]                            # ca 23 stelle
+moves = ["r", "u", "u", "l", "l", "l", "u", "l", "d", "r", "r", "r", "r", "d", "d", "l", "u", "r", "u", "l", "l", "l", "d", "d", "l", "l", "l", "u", "u", "r", "r", "d", "r", "d", "l", "u", "u", "u", "r", "d", "d"]
 
 def make_move(curr_pos: tuple, move: str) -> tuple:
     if move == "l":
@@ -30,22 +30,34 @@ def find_player_and_goal(curr_field):
     g = []
     for row_index in range(len(curr_field)):
         for col_index in range(len(curr_field[row_index])):
+            
             if curr_field[row_index][col_index] == "P":
                 p = (row_index, col_index)
+            
             if curr_field[row_index][col_index] == ".":
                 g.append((row_index, col_index))
+    
     return p, g
 
 
 def eval_move(curr_field, pos, curr_move):
     curr_pos = curr_field[pos[0]][pos[1]]
-    if curr_pos == "X":
+    
+    if curr_pos == "#":
         return -1, -1
-    elif curr_pos == " " or curr_pos == "$":
+    
+    elif curr_pos == " " or curr_pos == ".":
         return pos
-    elif curr_pos == ".":
-        stone_pos = make_move(pos, curr_move)
-        if
+    
+    elif curr_pos == "$":
+        stone_pos_row, stone_pos_col = make_move(pos, curr_move)
+        
+        if curr_field[stone_pos_row][stone_pos_col] == "#" or curr_field[stone_pos_row][stone_pos_col] == "$":  # wird angenommen dass immer nur 1 stein geschoben werden kann (falls 2 hinterinander stehen kann man nicht schieben)
+            return -1, -1
+        
+        else:
+            return stone_pos_row, stone_pos_col
+    
     else:
         return -1, -1
 
@@ -54,16 +66,39 @@ def workflow(game_field):
     player, goal_positions = find_player_and_goal(game_field)
     goal1_row, goal1_col = goal_positions[0]
     goal2_row, goal2_col = goal_positions[1]
-    for move in moves:
+    
+    for index, move in enumerate(moves):
         # wenn zielpunkte leer sind setzt ziel drauf
-        if game_field[goal1_row][goal1_col] == " ":
-            game_field[goal1_row][goal1_col] = "$"
-        if game_field[goal2_row][goal2_col] == " ":
-            game_field[goal2_row][goal2_col] = "$"
+        for goal_position in goal_positions:
+            if game_field[goal_position[0]][goal_position[1]] == " ":
+                game_field[goal_position[0]][goal_position[1]] == "." 
 
+        # fuehre naechsten zug aus und gib neue position zuruek
         next_pos = make_move(player, move)
         if next_pos[0] < 0 or next_pos[1] < 0:
             return False
-        next_pos = eval_move(game_field, next_pos, move)
-        if next_pos[0] < 0 or next_pos[1] < 0:
+        
+        # check ob naechste postion kein X ist
+        # wenn naechste position ein . ist fuehre dieses und vorherige funktion nochmal fuer punkt aus
+        stone_or_next_pos = eval_move(game_field, next_pos, move)
+        if stone_or_next_pos[0] < 0 or stone_or_next_pos[1] < 0:
             return False
+        
+        # wenn moves legal sind schiebe spieler 1 feld in moverichting mach altes feld zu leerem feld
+        game_field[player[0]][player[1]] = " "
+        game_field[next_pos[0]][next_pos[1]] = "P"
+
+        # wenn spieler stein geschoben hat setze stein ein feld weiter
+        if next_pos != stone_or_next_pos:
+            game_field[stone_or_next_pos[0]][stone_or_next_pos[1]] = "$"
+        
+        # setze neue spielerposition
+        player = next_pos
+
+
+    for goal_position in goal_positions:
+        if game_field[goal_position[0]][goal_position[1]] != "$":
+            return False
+    return True 
+
+print(workflow(field))
